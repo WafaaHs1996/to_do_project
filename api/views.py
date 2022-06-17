e8478d43b27740694db79ef1d08ca7e70dec5f97
WafaaHs1996 <wafaahs1996@gmail.com>
2022-06-17 17:20:20 +0300

    final delivery

--- api/views.py
+++ api/views.py
@@ -1,11 +1,48 @@
 #from django.shortcuts import render
+from lib2to3.pgen2 import token
 from rest_framework import generics, permissions
+from rest_framework.parsers import JSONParser
+from rest_framework.authtoken.models import Token
 from .serializers import *
 from todo.models import *
 
 from django.utils import timezone
+from django.db import IntegrityError
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate;
+
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 
 # Create your views here.
+@csrf_exempt
+def signup(request):
+    if request.method == 'POST':
+        try:
+            data = JSONParser().parse(request)
+            user = User.objects.create_user(data['username'], password=data['password'])
+            user.save()
+            token = Token.objects.create(user=user)
+            return JsonResponse({'token':str(token)} , status=201)
+        except IntegrityError:
+            return JsonResponse({'error':'That username has already been taken. Please choose a new username'} , status=400)
+
+
+@csrf_exempt
+def login(request):
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        user = authenticate(request, username=data['username'], password=data['password'])
+        if user is None:
+            return JsonResponse({'error':'Could not login. Please check username and password and try again.'} , status=400)
+        else:    
+            try:
+                token = Token.objects.get(user=user)
+            except:
+                token = Token.objects.create(user=user)    
+            return JsonResponse({'token':str(token)} , status=200)
+
+
 class TodoCompletedList(generics.ListAPIView):
      serializer_class = TodoSerializer
      permission_classes = [permissions.IsAuthenticated]
@@ -45,3 +82,4 @@ class TodoComplete(generics.UpdateAPIView):
      def perform_update(self, serializer):
           serializer.instance.datecompleted = timezone.now()
           serializer.save()
+
